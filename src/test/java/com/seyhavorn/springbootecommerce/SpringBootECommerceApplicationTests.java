04f123a9c0f7347e0f6dae4243805cc1642aa534
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2024-01-26 11:26:11 +0700

    init projec

---
+++ src/test/java/com/seyhavorn/springbootecommerce/SpringBootECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyhavorn.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
