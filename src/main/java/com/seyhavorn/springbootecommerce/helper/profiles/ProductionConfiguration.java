d27533e5b71dde3f764a30954f91a2ae7c4a0076
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2024-03-01 16:34:53 +0700

    Update Profile

--- src/main/java/com/seyhavorn/springbootecommerce/helper/profiles/ProductionConfiguration.java
+++ src/main/java/com/seyhavorn/springbootecommerce/helper/profiles/ProductionConfiguration.java
@@ -1,4 +1,4 @@
-package com.seyhavorn.springbootecommerce.profiles;
+package com.seyhavorn.springbootecommerce.helper.profiles;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
